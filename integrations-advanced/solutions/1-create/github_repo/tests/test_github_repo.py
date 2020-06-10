96e3ff19befc9663f48e033afce98699dd21f82f
ChristineTChen <christine.chen@datadoghq.com>
2020-06-10 18:03:18 -0400

    no solutions

--- integrations-advanced/solutions/1-create/github_repo/tests/test_github_repo.py
+++
@@ -1,9 +0,0 @@
-from datadog_checks.github_repo import GithubRepoCheck
-
-
-def test_check(instance):
-    check = GithubRepoCheck('github_repo', {}, {})
-    check.check(instance)
-
-    # In order to print debug logs we need to force the test to fail
-    assert False
--- integrations-advanced/solutions/6-advanced/github_repo/datadog_checks/github_repo/__init__.py
+++
@@ -1,4 +0,0 @@
-from .__about__ import __version__
-from .github_repo import GithubRepoCheck
-
-__all__ = ['__version__', 'GithubRepoCheck']
